defa02b0cdce5d996d271d7ac2c5f1f9eab27072
GK1312 <gkts1312@gmail.com>
2023-12-27 23:27:40 +0530

    project structure block

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      // enableTracing: true, // <-- debugging purposes only
+      useHash: true, // <-- for github pages
+      scrollPositionRestoration: 'top', // <-- scroll to top on route change
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
